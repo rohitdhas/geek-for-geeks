ed35a60c6e069d33a6d79e9549ee0b4c362b5fd5
Rohit Dhas <rohitdhas666@gmail.com>
2022-02-01 19:39:33 +0530

    updated title and link

--- array/equilibrium_point.cpp
+++ array/equilibrium_point.cpp
@@ -1,5 +1,5 @@
-// Problem - Leaders in an array (Easy)
-// Link - https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1/?category[]=Arrays&category[]=Arrays&difficulty[]=0&page=1&query=category[]Arraysdifficulty[]0page1category[]Arrays#
+// Problem - Equilibrium Point (Easy)
+// Link - https://practice.geeksforgeeks.org/problems/equilibrium-point-1587115620/1/?category[]=Arrays&category[]=Arrays&difficulty[]=0&page=2&query=category[]Arraysdifficulty[]0page2category[]Arrays
 
 #include <bits/stdc++.h>
 using namespace std;
